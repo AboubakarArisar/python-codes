869b66305cefbb00d5565953265b2162d0d123b1
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-02-07 17:17:44 +0500

    variables, data types in python

---
+++ Basics/basic.py
@@ -0,0 +1,21 @@
+print("Basics of python programming")
+
+# variables
+
+name = "Aboubakar"
+age = 20
+married = False
+height = 1.75
+weight = 70.5
+none = None
+print(name, age, married, height, weight, none)
+
+# Data types
+print(type(name))
+print(type(age))
+print(type(married))
+print(type(height))
+print(type(weight))
+print(type(none))
+
+
