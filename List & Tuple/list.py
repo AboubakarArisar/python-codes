b525db647b82ae518804cd1e31ef92e52d7dc3e3
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-02-07 23:51:25 +0500

    list in python

---
+++ List & Tuple/list.py
@@ -0,0 +1,19 @@
+#list
+
+info = ["Aboubakar", "Sukkur", 20, "Developer"]
+
+print(info[0])
+print(info[1])
+print(info[2])
+print(info[3])
+
+print(info)
+
+info[2] = 21
+#list is mutable
+print(info)
+
+#list slicing
+
+print(info[0:3])
+print(info[0:4])
