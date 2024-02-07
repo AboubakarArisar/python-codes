e7f783c2f2dfbc1325bb726a3423f5e60ca8a02a
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-02-07 19:16:13 +0500

    basic operations of string in python

---
+++ String/operations.py
@@ -0,0 +1,27 @@
+#concatination
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#repetition
+d = a * 3
+print(d)
+
+#length 
+e = len(a)
+print(e)
+
+#indexing
+f = a[1]
+print(f)
+#in indexing we can not assign the value to the index
+
+#slicing
+g = a[1:4]
+print(g)
+#in slicing last index is not included
+#in slicing negative indexing is also possible
+h = a[-3:-1]
+print(h)
