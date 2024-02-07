25dffc489b42eafa3c10ae1eb6956b0e24296bc1
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-02-07 23:57:27 +0500

    tuple in python and its methods

---
+++ List & Tuple/tuple.py
@@ -0,0 +1,20 @@
+#tuple
+
+# A tuple is a collection which is ordered and unchangeable. 
+#In Python tuples are written with round brackets.
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+# Access Tuple Items
+# You can access tuple items by referring to the index number, inside square brackets:
+
+print(thistuple[1])
+
+# slicing
+
+print(thistuple[1:3])
+
+# index
+
+print(thistuple.index("banana"))
