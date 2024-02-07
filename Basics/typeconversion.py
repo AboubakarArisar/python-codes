81e8d9f09d73ee0d214c00a4f50c072f0b385684
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-02-07 18:48:20 +0500

    type conversion in python

---
+++ Basics/typeconversion.py
@@ -0,0 +1,31 @@
+#type conversion
+
+#int to float
+a = 10
+b = float(a)
+print(b)
+
+#float to int
+c = 10.5
+d = int(c)
+print(d)
+
+#int to string
+e = 10
+f = str(e)
+print(f)
+
+#string to int
+g = "10"
+h = int(g)
+print(h)
+
+#float to string
+i = 10.5
+j = str(i)
+print(j)
+
+#string to float
+k = "10.5"
+l = float(k)
+print(l)
