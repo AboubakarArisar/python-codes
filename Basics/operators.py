e3e9f3350abba54c8721ec0bbe44fa4073c7f9f3
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-02-07 17:28:55 +0500

    operators in python

---
+++ Basics/operators.py
@@ -0,0 +1,73 @@
+#operaors
+#Arithmetic operators
+
+#Addition
+print("Addiion : ", 2+3)
+
+#Subtraction
+print("Substraction : ",2-3)
+
+#Multiplication
+print("Multiplication : ",2*3)
+
+#Division
+print("Division : ",2/3)
+
+#Modulus
+print("Modulus : ",2%3)
+
+
+#Logical operators
+#And
+print("And : ", True and False)
+
+#Or
+print("Or : ", True or False)
+
+#Not
+print("Not : ", not True)
+
+#Comparison operators
+#Equal
+print("Equal : ", 2==3)
+
+#Not equal
+print("Not equal : ", 2!=3)
+
+#Greater than
+print("Greater than : ", 2>3)
+
+#Less than
+print("Less than : ", 2<3)
+
+#Greater than or equal to
+print("Greater than or equal to : ", 2>=3)
+
+#Less than or equal to
+print("Less than or equal to : ", 2<=3)
+
+#Assignment operators
+#Assign
+x = 5
+print("Assign : ", x)
+
+#Add and assign
+x += 3
+print("Add and assign : ", x)
+
+#Subtract and assign
+x -= 3
+print("Subtract and assign : ", x)
+
+#Multiply and assign
+x *= 3
+print("Multiply and assign : ", x)
+
+#Divide and assign
+x /= 3
+print("Divide and assign : ", x)
+
+#Modulus and assign
+x %= 3
+print("Modulus and assign : ", x)
+
