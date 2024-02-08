5961a68a8357c39c49a27a72988bfd64fe005cab
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-02-08 18:25:07 +0500

    practice questions of dictionary and set

---
+++ Dictionary & set/task.py
@@ -0,0 +1,11 @@
+words_meaning = {"table": ("a piece of furniture" , "list of data"), "cat" : "a small animal"}
+
+print(words_meaning)
+
+marks = {}
+
+marks["math"] = 90
+marks["english"] = 80
+marks["science"] = 85
+
+print(marks)
