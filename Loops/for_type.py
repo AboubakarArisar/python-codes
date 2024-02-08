2929b4368a9eb4743192b9851fd93b903c76a059
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-02-09 00:45:51 +0500

    format of for loop in python

---
+++ Loops/for_type.py
@@ -0,0 +1,15 @@
+#for loop
+
+for i in range(5):
+    print(i)
+
+#for loop with start and end
+    
+for i in range(2,5):
+    print(i)
+
+#for loop with start, end and step
+    
+for i in range(2,10,2):
+    print(i)        
+
