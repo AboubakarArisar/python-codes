4cced5f8d193fc34935ad713b48a9cb6a343cc21
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-02-09 00:58:00 +0500

    pass statement in python

---
+++ Loops/pass.py
@@ -0,0 +1,8 @@
+#pass in python
+
+for i in range(10):
+    pass
+
+print("This is a pass statement")
+# pass is a null operation -- when it is executed, nothing happens. It is useful as a placeholder
+# when a statement is required syntactically, but no code needs to be executed, for example:
