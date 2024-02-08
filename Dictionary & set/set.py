24382b064068ed6fa8bd2ee72de9175c5e4a4bcf
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-02-08 17:33:24 +0500

    sets in python

---
+++ Dictionary & set/set.py
@@ -0,0 +1,17 @@
+#sets in python
+
+#sets are unordered collection of unique elements
+
+name = {"aboubakar", "arisar", "sukkur", "sukkur"}
+
+#set ignores the duplicate elements
+
+print(name)
+
+
+#to check if an element is in the set
+print("aboubakar" in name)
+
+#to create an empty set
+
+empty_set = set()
