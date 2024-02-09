6a0b2309e0353bf9ee273e39dedaea3b20e6fa4c
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-02-10 00:28:03 +0500

    writing in the file with existing stuff

---
+++ Filing/append.py
@@ -0,0 +1,12 @@
+#opening file in write mode
+
+file = open("ab.txt", "a")
+
+#writing in the file as appending means adding in the existing file
+
+file.write("\nthis is added as new line")
+
+#closing the file 
+
+
+file.close()
