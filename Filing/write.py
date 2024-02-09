da0c1c7811f92586f6e421aa9358f90796df7c6d
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-02-10 00:24:20 +0500

    writing in the file in python

---
+++ Filing/write.py
@@ -0,0 +1,11 @@
+#opening file in write mode
+
+file = open("ab.txt", "w")
+#writing in the file
+
+file.write("this is the overwritten text using python in txt file")
+
+#closing the file 
+
+
+file.close()
